1a4004117ab19a00db29034ed7743bd2fb194d10
Sahrane_Guassemi <sahranedev@gmail.com>
2023-06-07 16:40:16 +0200

    add update method

--- backend/src/routes/patientForm.js
+++ backend/src/routes/patientForm.js
@@ -4,13 +4,13 @@ const router = express.Router();
 
 const createPatientFormController = require("../controllers/patientFormControllers/CreatePatientFormController");
 const readOnePatientFormController = require("../controllers/patientFormControllers/ReadOnePatientFormController");
-const updateUserController = require("../controllers/userControllers/UpdateUserController");
+const updatePatientFormController = require("../controllers/patientFormControllers/UpdatePatientFormController");
 const readPatientForm = require("../controllers/patientFormControllers/ReadPatientFormController");
 const deletePatientFormController = require("../controllers/patientFormControllers/DeletePatientFormController");
 
 router.get("/", readPatientForm);
 router.get("/:id", readOnePatientFormController);
-router.put("/:id", updateUserController);
+router.put("/:id", updatePatientFormController);
 router.post("/", createPatientFormController);
 router.delete("/:id", deletePatientFormController);
 
