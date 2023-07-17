f4e86561fd11dd1635576b24a51e7c4e442d48e1
A L <ultimaterror@laposte.net>
2023-07-17 11:30:42 +0200

    Checklist by Intervention

--- backend/src/routes/doneCheckList.js
+++ backend/src/routes/doneCheckList.js
@@ -2,11 +2,18 @@ const express = require("express");
 
 const router = express.Router();
 
+const ReadByInterventionIdDoneCheckListController = require("../controllers/doneCheckListController/ReadByInterventionIdDoneCheckListController");
+
 const CreateDoneCheckListController = require("../controllers/doneCheckListController/CreateDoneCheckListController");
 const ReadDoneCheckListController = require("../controllers/doneCheckListController/ReadDoneCheckListController");
 const UpdateDoneCheckListController = require("../controllers/doneCheckListController/UpdateDoneCheckListController");
 const ReadOneDoneCheckListController = require("../controllers/doneCheckListController/ReadOneDoneCheckListController");
 const DeleteDoneCheckListController = require("../controllers/doneCheckListController/DeleteDoneCheckListController");
+
+router.get(
+  "/byInterventionId/:interventionId",
+  ReadByInterventionIdDoneCheckListController
+);
 
 router.get("/", ReadDoneCheckListController);
 router.get("/:id", ReadOneDoneCheckListController);
--- frontend/src/pages/ArrivalQuiz.jsx
+++ frontend/src/pages/ArrivalQuiz.jsx
@@ -1,18 +1,10 @@
 import React, { useState } from "react";
-import { Box, Typography } from "@mui/material/";
+import { Box } from "@mui/material/";
 
 export default function ArrivalQuiz() {
   const [steps, setSteps] = useState(2);
   return (
-    <Box
-      sx={{
-        width: "90%",
-        backgroundColor: "#F6F4F4",
-        marginTop: 3,
-        borderRadius: 2,
-        p: 2,
-      }}
-    >
+    <Box>
       {steps === 1 && <p>AAAAAAAAAAAAAA</p>}
       {steps === 2 && <p>BBBBBBBBBBBBBBB</p>}
       {steps === 3 && <p>CCCCCCCCCCCCCCC</p>}
