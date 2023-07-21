85dd6afd40a8e50c1aa01d36d4af594f9b2ce7be
A L <ultimaterror@laposte.net>
2023-07-21 09:27:43 +0200

    Patient Header

--- frontend/src/components/PatientNavigation.jsx
+++ frontend/src/components/PatientNavigation.jsx
@@ -1,18 +1,58 @@
 import * as React from "react";
 import { Box, Typography } from "@mui/material/";
 import PatientButton from "@components/PatientButton";
-import { Outlet, useLocation, useOutlet, Link } from "react-router-dom";
+import {
+  Outlet,
+  useLocation,
+  useOutlet,
+  Link,
+  useNavigate,
+} from "react-router-dom";
 import useMediaQuery from "@mui/material/useMediaQuery";
 
-// import { useCurrentUserContext } from "../Context/UserContext";
+import ApiHelper from "@services/ApiHelper";
+import { useCurrentUserContext } from "../Context/UserContext";
+import HeaderPatient from "./HeaderPatient";
+import HeaderPatientMobile from "./HeaderPatientMobile";
+
+const defaultProgression = {
+  interventionId: 0,
+  interventionDate: null,
+  rsiDone: 0,
+  rsiMax: 0,
+  daDone: 0,
+  daMax: 0,
+  rapDone: 0,
+  afiDone: 0,
+  afiMax: 0,
+  dclDone: 0,
+  dclMax: 0,
+};
 
 export default function PatientNavigation() {
-  // const { user } = useCurrentUserContext();
+  const { user, token } = useCurrentUserContext();
+  const [progression, setProgression] = React.useState(defaultProgression);
+  const [reload, setReload] = React.useState(false);
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    ApiHelper(`interventions/progression/${user.id}`, "GET", null, token)
+      .then((response) => {
+        if (response.status !== 200) {
+          navigate("/no-intervention");
+        }
+        return response.json();
+      })
+      .then((result) => {
+        setProgression(result);
+      })
+      .catch(() => {});
+  }, [reload]);
+
   const hasOutlet = useOutlet();
   const isMobile = useMediaQuery("(max-width:600px)");
 
   const location = useLocation().pathname;
-
   return (
     <Box
       sx={{
@@ -20,83 +60,97 @@ export default function PatientNavigation() {
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
+        // backgroundColor: isMobile && "#040055",
+        minHeight: "100vh",
       }}
     >
-      {isMobile && hasOutlet && (
-        <Typography
-          sx={{ alignSelf: "flex-start", marginX: 3, marginY: 2 }}
-          component={Link}
-          to="/patient"
-        >
-          {"< "}
-          Retour{" "}
-        </Typography>
+      {isMobile ? (
+        <HeaderPatientMobile progression={progression} />
+      ) : (
+        <HeaderPatient
+          firstname={user.firstname}
+          lastname={user.lastname}
+          interventionDate={progression.interventionDate}
+        />
       )}
       <Box
         sx={{
+          width: "100%",
+          height: "100%",
           display: "flex",
-          justifyContent: "space-between",
+          flexDirection: "column",
           alignItems: "center",
-          width: "90%",
-          flexDirection: { xs: "column", sm: "row" },
+          backgroundColor: "white",
+          borderTopLeftRadius: 34,
+          borderTopRightRadius: 34,
         }}
       >
-        <PatientButton
-          color="#F5D23F"
-          title="Comprendre mon opération"
-          total={9}
-          done={8}
-          selected={(!hasOutlet && isMobile) || location.includes("info-op")}
-          link="info-op"
-        />
-        <PatientButton
-          color="#47CACF"
-          title="Démarche administrative"
-          total={5}
-          done={2}
-          selected={
-            (!hasOutlet && isMobile) || location.includes("administrative")
-          }
-          link="administrative"
-        />
-        <PatientButton
-          color="#FD93B8"
-          title="Préparer mon arrivée"
-          total={8}
-          done={3}
-          selected={(!hasOutlet && isMobile) || location.includes("quiz")}
-          link="quiz"
-        />
-        <PatientButton
-          color="#66E47A"
-          title="Prendre rendez-vous"
-          total={5}
-          done={2}
-          selected={(!hasOutlet && isMobile) || location.includes("take-rdv")}
-          link="take-rdv"
-        />
-        <PatientButton
-          color="#8D77F0"
-          title="Check-list avant le départ"
-          total={5}
-          done={2}
-          selected={(!hasOutlet && isMobile) || location.includes("checklist")}
-          link="checklist"
-        />
-      </Box>
-      {hasOutlet && (
+        {isMobile && hasOutlet && (
+          <Typography
+            sx={{ alignSelf: "flex-start", marginX: 3, marginY: 2 }}
+            component={Link}
+            to="/patient"
+          >
+            {"< "}
+            Retour{" "}
+          </Typography>
+        )}
         <Box
           sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
             width: "90%",
-            backgroundColor: !isMobile && "#F6F4F4",
-            marginTop: 3,
-            borderRadius: 2,
-            p: 4,
+            flexDirection: { xs: "column", sm: "row" },
           }}
         >
-          <Outlet />
+          <PatientButton
+            color="#F5D23F"
+            title="Comprendre mon opération"
+            total={progression.rsiMax}
+            done={progression.rsiDone}
+            selected={(!hasOutlet && isMobile) || location.includes("info-op")}
+            link="info-op"
+          />
+          <PatientButton
+            color="#47CACF"
+            title="Démarches administratives"
+            total={progression.daMax}
+            done={progression.daDone}
+            selected={
+              (!hasOutlet && isMobile) || location.includes("administrative")
+            }
+            link="administrative"
+          />
+          <PatientButton
+            color="#FD93B8"
+            title="Préparer mon arrivée"
+            total={5}
+            done={progression.rapDone}
+            selected={(!hasOutlet && isMobile) || location.includes("quiz")}
+            link="quiz"
+          />
+          <PatientButton
+            color="#66E47A"
+            title="Prendre rendez-vous"
+            total={progression.afiMax}
+            done={progression.afiDone}
+            selected={(!hasOutlet && isMobile) || location.includes("take-rdv")}
+            link="take-rdv"
+          />
+          <PatientButton
+            color="#8D77F0"
+            title="Check-list avant le départ"
+            total={progression.dclMax}
+            done={progression.dclDone}
+            selected={
+              (!hasOutlet && isMobile) || location.includes("checklist")
+            }
+            link="checklist"
+          />
         </Box>
-      )}
+        <Outlet context={[reload, setReload, progression.interventionId]} />
+      </Box>
     </Box>
   );
 }
--- frontend/src/pages/TakeRDV.jsx
+++ frontend/src/pages/TakeRDV.jsx
@@ -1,5 +1,21 @@
+import { Box, useMediaQuery } from "@mui/material";
 import * as React from "react";
 
 export default function TakeRDV() {
-  return <div>Page4</div>;
+  const isMobile = useMediaQuery("(max-width:600px)");
+
+  return (
+    <Box
+      sx={{
+        width: "90%",
+        backgroundColor: !isMobile && "#F6F4F4",
+        marginTop: 3,
+        borderRadius: 4,
+        p: 4,
+        border: !isMobile && `3px solid #66E47A`,
+      }}
+    >
+      <p>p4</p>
+    </Box>
+  );
 }
